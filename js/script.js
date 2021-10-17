cb1096f8235de842f61216dc0f533d778991b567
dinara13 <83403175+dinara409@users.noreply.github.com>
2021-10-17 21:19:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        adaptiveHeight: true,
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_main.png" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_main.png" alt="right"></button>'
+    });
+});
+
